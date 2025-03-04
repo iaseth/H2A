1e64875c422194bab5a571a9ccaf0d455efb4dce
Ankur Seth <iankurseth@gmail.com>
2025-03-04 14:12:42 +0530

    Removed filenames, now using default

--- H2A.sh
+++ H2A.sh
@@ -2,7 +2,7 @@ count=3600
 wait=10
 for i in $(seq $count); do
     echo "Taking screenshot #${i} ..."
-    gnome-screenshot -f foo.png
+    gnome-screenshot
     echo "\twaiting for $wait seconds ..."
     sleep $wait
 done
